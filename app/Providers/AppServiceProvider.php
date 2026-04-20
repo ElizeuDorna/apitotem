e97e42a958331008bc86adb9a7f3133249d59f17
Elizeu Dorna <elizeupedrinho@hotmail.com>
2026-04-20 17:21:33 -0400

    Set application locale to Portuguese

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        config([
+            'app.locale' => 'pt_BR',
+            'app.fallback_locale' => 'pt_BR',
+        ]);
+
+        App::setLocale('pt_BR');
     }
 }
---
+++ lang/pt_BR/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'reset' => 'Sua senha foi redefinida com sucesso.',
+    'sent' => 'Enviamos para o seu e-mail o link de redefinicao de senha.',
+    'throttled' => 'Aguarde antes de tentar novamente.',
+    'token' => 'Este token de redefinicao de senha e invalido.',
+    'user' => 'Nao encontramos um usuario com esse endereco de e-mail.',
+];
