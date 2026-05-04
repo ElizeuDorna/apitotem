ee7b928cc3580f72d977e05a01fe78f772d94128
Elizeu Dorna <elizeupedrinho@hotmail.com>
2026-05-04 17:21:08 -0400

    Use configured panel icon on guest auth pages

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,9 +1,31 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        @php
+            $guestPanelBrandIconUrl = '';
+            $guestEmpresaId = (int) (session(\App\Support\EmpresaContext::ADMIN_SESSION_KEY)
+                ?: session(\App\Support\EmpresaContext::SESSION_KEY));
+            $hasPanelBrandIconColumn = \Illuminate\Support\Facades\Schema::hasColumn('configuracoes', 'panelBrandIconUrl');
+
+            if ($hasPanelBrandIconColumn) {
+                $guestPanelBrandIconQuery = \App\Models\Configuracao::query()
+                    ->whereNotNull('panelBrandIconUrl')
+                    ->where('panelBrandIconUrl', '!=', '');
+
+                if ($guestEmpresaId > 0) {
+                    $guestPanelBrandIconQuery->where('empresa_id', $guestEmpresaId);
+                }
+
+                $guestPanelBrandIconUrl = (string) ($guestPanelBrandIconQuery
+                    ->orderByDesc('id')
+                    ->value('panelBrandIconUrl') ?? '');
+            }
+        @endphp
+
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="icon" href="{{ $guestPanelBrandIconUrl !== '' ? $guestPanelBrandIconUrl : asset('favicon.ico') }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
@@ -18,7 +40,11 @@
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
                 <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    @if ($guestPanelBrandIconUrl !== '')
+                        <img src="{{ $guestPanelBrandIconUrl }}" alt="Ícone do painel" class="h-20 w-20 object-contain">
+                    @else
+                        <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    @endif
                 </a>
             </div>
 
